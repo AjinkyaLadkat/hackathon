648680cafe6807bcaaea2b0964526e9b1e1b047b
prathamesh7652 <nprathamesh7652@gmail.com>
2024-02-12 12:41:32 +0530

    vishal kallya

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('movingButton');
-
-document.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-
-    button.style.left = mouseX + 'px';
-    button.style.top = mouseY + 'px';
-});
